20b4d9df5da198f38ece3d8b1cd82b98a2e7330d
Zen Yu  Ong <ongzenyu112@gmail.com>
2022-06-30 20:33:18 +0800

    web article route

--- src/client/src/App.js
+++ src/client/src/App.js
@@ -1,13 +1,15 @@
 import React from "react";
 import { Route, Routes } from "react-router-dom";
+import WebArtlcle from "./pages/webArticle";
 import Home from "./pages/Home";
 
 function App() {
   return (
     <>
-      <h1>HEAP IS FAKE NEWS</h1>;
+      <h1>HEAP IS FAKE NEWS</h1>
       <Routes>
         <Route path="/" element={<Home />} />
+        <Route path="/article" element={<WebArtlcle />} />
       </Routes>
     </>
   );
---
+++ src/client/src/pages/webArticle.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+export default function WebArticle() {
+  return (
+    <>
+      <h1>FAX OR CAP</h1>
+      <h2>Validate Your News in Seconds</h2>
+      <input type="text" name="url" placeholder="Your Link Here"></input>
+      <input type="submit" value="Check validity"></input>
+    </>
+  );
+}
