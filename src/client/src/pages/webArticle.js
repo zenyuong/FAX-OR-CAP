b6f61b5f7cf464dc8afa44f37ee4fe97603acd59
Zen Yu  Ong <ongzenyu112@gmail.com>
2022-07-01 14:41:31 +0800

    basic skeleton of webArticle page

--- src/client/src/pages/webArticle.js
+++ src/client/src/pages/webArticle.js
@@ -1,12 +1,36 @@
-import React from "react";
+import React, { useRef, useState, Fragment } from "react";
 
 export default function WebArticle() {
+  const URL = useRef();
+  const [toggleResults, setToggleResults] = useState(false);
+
+  function handleSubmit(event) {
+    event.preventDefault();
+    setToggleResults(!toggleResults);
+    console.log(URL.current?.value);
+  }
   return (
     <>
-      <h1>FAX OR CAP</h1>
-      <h2>Validate Your News in Seconds</h2>
-      <input type="text" name="url" placeholder="Your Link Here"></input>
-      <input type="submit" value="Check validity"></input>
+      <Fragment>
+        {toggleResults === false ? (
+          <>
+            <h1>FAX OR CAP</h1>
+            <h2>Validate Your News in Seconds</h2>
+            <form onSubmit={handleSubmit}>
+              <input
+                type="text"
+                name="url"
+                placeholder="Your Link Here"
+                ref={URL}
+              ></input>
+              <br />
+              <input type="submit" value="Check validity"></input>
+            </form>
+          </>
+        ) : (
+          <h1>Results</h1>
+        )}
+      </Fragment>
     </>
   );
 }
