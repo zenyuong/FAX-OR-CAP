dc2e8c5aafe708c31d8d2d39387d6b4b16aad844
Arthur Hain <arthur.hain.2020@scis.smu.edu.sg>
2022-07-04 11:32:46 +0800

    Converted to React Css + HTML
    @zenyuong help me check if i did it correctly. I'm not sure if what i did are considered best practices!

--- src/client/src/App.js
+++ src/client/src/App.js
@@ -1,12 +1,13 @@
 import React from "react";
 import { Route, Routes } from "react-router-dom";
 import WebArtlcle from "./pages/webArticle";
-import Home from "./pages/Home";
+import Home from "./pages/home/Home";
+
+
 
 function App() {
   return (
     <>
-      <h1>HEAP IS FAKE NEWS</h1>
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/article" element={<WebArtlcle />} />
