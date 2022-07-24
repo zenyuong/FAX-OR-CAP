747ff84e3665906018e2170eb22e1a9facd4f109
Zen Yu  Ong <ongzenyu112@gmail.com>
2022-07-24 17:15:36 +0800

    no changes

--- src/client_updated/src/components/BodyURL.js
+++ src/client_updated/src/components/BodyURL.js
@@ -5,7 +5,7 @@ import axios from "axios";
 export default function BodyURL() {
   const URL = useRef();
   const [loading, setLoading] = useState(false);
-  
+
   // CALL TO NODE.JS
   const handleSubmit = (e) => {
     e.preventDefault();
@@ -28,15 +28,23 @@ export default function BodyURL() {
     return () => cancel();
   };
 
+  if (loading) return "loading...";
+
   return (
     <>
       <div className="body-container">
         <div className="body-icons">
-          <img src="globe.png" alt="globe" width="130" height="130"/>
+          <img src="globe.png" alt="globe" width="130" height="130" />
         </div>
         <div className="input-box">
           <form onSubmit={handleSubmit}>
-            <input className="input-box-url" type="text" size={80} placeholder="Enter URL" ref={URL} />
+            <input
+              className="input-box-url"
+              type="text"
+              size={80}
+              placeholder="Enter URL"
+              ref={URL}
+            />
             <i className="fa-solid fa-link fa-lg"></i>
             <br />
             <button type="submit">Check Validity</button>
