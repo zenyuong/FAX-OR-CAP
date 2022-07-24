f4bf3d6c9fd3bd31310b432f37d9b37033f44712
Zen Yu  Ong <ongzenyu112@gmail.com>
2022-07-24 11:57:00 +0800

    loading + cancel token

--- src/client_updated/src/components/Body.js
+++ src/client_updated/src/components/Body.js
@@ -1,21 +1,33 @@
-import React, { useRef } from "react";
+import React, { useRef, useState } from "react";
 import "./Body.css";
 import axios from "axios";
 
 export default function Body() {
   const URL = useRef();
+  const [loading, setLoading] = useState(false);
 
   const handleSubmit = (e) => {
     e.preventDefault();
     const Link = URL.current?.value;
+    setLoading(true);
+    let cancel;
+
     axios
       .post("http://localhost:1010/article/action", {
+        method: "POST",
         body: JSON.stringify({ URL: Link }),
+        cancelToken: new axios.CancelToken((c) => {
+          cancel = c;
+        }),
       })
       .then((res) => {
+        setLoading(false);
         console.log(res);
       });
+    return () => cancel();
   };
+
+  if (loading) return "loading...";
 
   return (
     <>
