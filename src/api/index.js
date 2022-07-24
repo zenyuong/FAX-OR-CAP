7d39d2110762bbce742206bf6461a87040a4181b
Zen Yu  Ong <ongzenyu112@gmail.com>
2022-07-24 16:57:16 +0800

    no changes

--- src/api/index.js
+++ src/api/index.js
@@ -25,7 +25,6 @@ function initAPIServer() {
   });
 
   app.use("/article", require("./routers/webArtlcle")());
-
   app.use("/twitter", require("./routers/twitterAPI")());
 
   let listener = app.listen(process.env.PORT || 1010, () => {
