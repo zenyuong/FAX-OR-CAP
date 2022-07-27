48ca698d24d57adf68b9114eb6a57844982a3e13
Zen Yu  Ong <ongzenyu112@gmail.com>
2022-07-27 22:07:56 +0800

    removed uneeded dependency

--- src/client_updated/src/components/BodyQueries.js
+++ src/client_updated/src/components/BodyQueries.js
@@ -1,6 +1,5 @@
 import axios from "axios";
 import React, { useState, useEffect } from "react";
-import { Link } from "react-router-dom";
 import "../styles/Body.css";
 
 export default function BodyQueries() {
