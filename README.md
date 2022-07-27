cf856f7e69b9aa9e12cc1164b2ccb01c2d64355e
Zen Yu  Ong <ongzenyu112@gmail.com>
2022-07-27 20:41:34 +0800

    moved directory

--- README.md
+++ README.md
@@ -66,7 +66,7 @@ node index.js
 ```
 ### User Scenario 1: User wants to check the validity of a news piece
 1. User starts from the landing page   
-![Landing Page](https://github.com/zenyuong/HEAP-IS-FAKE-NEWS-/blob/efee5408061c944d3d14bf730a48cf20536bcafd/home_url.jpeg)  
+![Landing Page](https://github.com/zenyuong/HEAP-IS-FAKE-NEWS-/screenshots/blob/efee5408061c944d3d14bf730a48cf20536bcafd/home_url.jpeg)  
 2. User clicks on the fake news detection icon  
 3. User inserts the HTML link of their choice into the input box  
 ![Adding in a URL](https://github.com/zenyuong/HEAP-IS-FAKE-NEWS-/blob/efee5408061c944d3d14bf730a48cf20536bcafd/url_page.jpeg)  
--- src/client_updated/src/components/BodyQueries.js
+++ src/client_updated/src/components/BodyQueries.js
@@ -7,40 +7,43 @@ export default function BodyQueries() {
   const [queryOption, setQueryOption] = useState("recent");
 
   useEffect(() => {
-    axios.get(`http://localhost:1010/article/${queryOption}`)
-    .then((res) => {
-      console.log(res.data);
-      setQueries(res.data);
-    })
-    .catch(err => console.log(err))
+    axios
+      .get(`http://localhost:1010/article/${queryOption}`)
+      .then((res) => {
+        console.log(res.data);
+        setQueries(res.data);
+      })
+      .catch((err) => console.log(err));
   }, [queryOption]);
 
-  if(queries!==undefined){
-    let count = 0
+  if (queries !== undefined) {
+    let count = 0;
     var result = queries.map((query, idx) => {
-      return(
+      return (
         <ul>
           <li key={idx}>
-            <span className="query-order">{count+=1}</span>
+            <span className="query-order">{(count += 1)}</span>
             <span className="query-title">{`"${query.title}"`}</span>
           </li>
         </ul>
-      )
-    })
+      );
+    });
   }
 
   return (
     <>
       <div>
-        <select className="query-selector" name="queryOption" onChange={e=>setQueryOption(e.target.value)}>
+        <select
+          className="query-selector"
+          name="queryOption"
+          onChange={(e) => setQueryOption(e.target.value)}
+        >
           <option value="recent">Most Recent</option>
           <option value="popular">Most Popular</option>
         </select>
       </div>
       <div className={"queries-wrapper"}>
-        <div className={"queries"}>
-          {result}
-        </div>
+        <div className={"queries"}>{result}</div>
       </div>
     </>
   );
